1999e69180d93a48ea96adbb8ca7e46f8b1b3d90
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-12 19:20:48 +0530

    added animation in the hero section

--- app.js
+++ app.js
@@ -9,7 +9,111 @@ function mouseFollower() {
     window.addEventListener("mousemove", function (e) {
         let bubble = document.querySelector('.bubble');
         bubble.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`
-    })
+    });
 }
 
 mouseFollower();
+
+let tl = gsap.timeline();
+
+gsap.from('.port', {
+    opacity: 0,
+    y: 40,
+    duration: 0.5,
+    delay: .5
+})
+
+gsap.from('.navLinks a', {
+    opacity: 0,
+    y: 40,
+    duration: 0.5,
+    delay: .5,
+    stagger: .3
+})
+
+
+tl.from('.hero-details h1', {
+    opacity: 0,
+    y: 100,
+    duration: .3,
+    delay: 1,
+    ease: 'easeInOut'
+})
+
+tl.from('.hero-details h2', {
+    opacity: 0,
+    y: 100,
+    duration: .3,
+    ease: 'easeInOut'
+
+})
+
+tl.from('.hero-details p', {
+    opacity: 0,
+    y: 100,
+    duration: .3,
+    ease: 'easeInOut'
+
+})
+
+tl.from('.viewwork', {
+    opacity: 0,
+    y: 100,
+    duration: .3,
+
+})
+
+
+tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.about .h1text h1 ',
+        scroller: 'body',
+        start: 'top 80%',
+        end: "top 30%",
+        toggleActions: 'play none none reverse', // controls when it plays/reverses
+        // time for animation // start earlier so you see the animation
+        scrub: 2, // uncomment if you want scroll-linked animation
+
+    }
+});
+
+tl.from('.about .h1text h1', {
+    opacity: 0,
+    x: -400,
+    duration: 1,
+    ease: 'power3.out'
+});
+
+gsap.from('.divimg', {
+    scrollTrigger: {
+        trigger: '.divimg',
+        start: 'top 80%',
+        end: "top 50%",
+        scroller: 'body',
+        toggleActions: 'play none none reverse', // controls when it plays/reverses
+        // time for animation // start earlier so you see the animation
+        scrub: 2,
+    },
+    opacity: 0,
+    x: -400,
+    duration: 0.8,
+    ease: 'power3.out'
+});
+
+tl.from('.techStack', {
+    opacity: 0,
+    x: 70,
+})
+
+
+tl.from('.aboutText', {
+    opacity: 0,
+    x: 400,
+})
+
+
+
+
+
+
+
